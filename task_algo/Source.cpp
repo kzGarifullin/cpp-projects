576e3b64fa1d300f15bead56709a6df20c7c1ef3
Kamil Garifullin <kamil_20.03@mail.ru>
2021-02-28 15:21:21 +0300

    add 5

--- task_algo/Source.cpp
+++ task_algo/Source.cpp
@@ -22,23 +22,36 @@
 
 int main(int argc, char** argv)
 {
-	std::vector < std::string > vector;
+	//std::vector < std::string > vector;
+	std::vector<int>vector{ 1,2,3,4,5,6,7,8,9,10,11 };//#1
+	
+	
+	//std::copy(
+	//	std::istream_iterator < int >(std::cin),//#2
+	//	std::istream_iterator < int >(),
+	//	std::back_inserter(vector));
 
-	std::copy(
-		std::istream_iterator < std::string >(std::cin),
-		std::istream_iterator < std::string >(),
-		std::back_inserter(vector));
+
 	std::mt19937 g;
 	//std::sort(vector.begin(), vector.end());
-	std::shuffle(vector.begin(), vector.end(),g);
-	std::sort(vector.begin(), vector.end());
-	std::unique_copy(vector.cbegin(), vector.cend(),
-		std::ostream_iterator < std::string >(std::cout, "\n"));
-	std::shuffle(vector.begin(), vector.end(), g);
-	std::cout << std::endl;
+	std::cout << "shuffle"<<std::endl;
+	std::shuffle(vector.begin(), vector.end(),g);//#3
 	std::copy(vector.cbegin(), vector.cend(),
-		std::ostream_iterator < std::string >(std::cout, "\n"));
-
+	std::ostream_iterator < int >(std::cout, "\n"));
+	std::sort(vector.begin(), vector.end());
+	std::cout << "unique"<<std::endl;
+	std::unique_copy(vector.cbegin(), vector.cend(),//#4
+		std::ostream_iterator < int >(std::cout, "\n"));
+	
+	std::cout << std::endl;
+	
+	std::cout << "amount of odd numbers: "<<std::count_if(std::begin(vector), 
+		std::end(vector), [](auto x) {return x % 2; })<<std::endl;//#5
+	auto [min, max] = std::minmax_element(std::begin(vector),std::end(vector));
+	std::cout << "min: "<<*min<<". max: "<<*max << std::endl;//#6
+	
+	
+	
 	system("pause");
 
 	return EXIT_SUCCESS;
