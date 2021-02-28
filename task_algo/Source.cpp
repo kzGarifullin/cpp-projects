0886ca35a0d96fe46357bbd75696466b43a2a488
Kamil Garifullin <kamil_20.03@mail.ru>
2021-02-28 19:00:31 +0300

    19 task added

--- task_algo/Source.cpp
+++ task_algo/Source.cpp
@@ -90,14 +90,48 @@ int main(int argc, char** argv)
 	std::for_each(std::begin(v3), std::end(v3), [](auto x) {std::cout << x << " "; });
 	std::cout << std::endl;
 
-	std::for_each(std::begin(v3), std::end(v3), [](auto &x) {if (x < 50) x = 0; });//#13
+	std::for_each(std::begin(v3), std::end(v3), [](auto &x) {if (x < 30) x = 0; });//#13
 	std::for_each(std::begin(v3), std::end(v3), [](auto x) {std::cout << x << " "; });
 	std::cout << std::endl;
 	
 	v3.erase(std::remove_if(std::begin(v3), std::end(v3), [](auto x) {return x == 0; }), std::end(v3));
 	std::for_each(std::begin(v3), std::end(v3), [](auto x) {std::cout << x << " "; });
 	std::cout << std::endl;//#14
-	system("pause");
+	
+
+	std::cout << "15 task"<<std::endl;
+	std::reverse(std::begin(v3), std::end(v3));//#15
+	std::for_each(std::begin(v3), std::end(v3), [](auto x) {std::cout << x << " "; });
+	std::cout << std::endl;
+	
+	std::cout << "16 task" << std::endl;
+	std::nth_element(std::begin(v3), std::next(std::begin(v3), 3), std::end(v3), std::greater<int>());
+	std::for_each(std::begin(v3), std::end(v3), [](auto x) {std::cout << x << " "; });
+	std::cout << std::endl;
+
+	std::cout << "17 task" << std::endl;
+	std::sort(std::begin(v2), std::end(v2));
+	std::sort(std::begin(vector), std::end(vector));
+	std::for_each(std::begin(v2), std::end(v2), [](auto x) {std::cout << x << " "; });
+	std::cout << std::endl;
+	std::for_each(std::begin(vector), std::end(vector), [](auto x) {std::cout << x << " "; });
+	std::cout << std::endl;
+
+
+	std::cout << "18 task" << std::endl;
+	std::vector<int> v4;
+	std::merge(vector.begin(), vector.end(), v2.begin(), v2.end(),std::back_inserter(v4));
+	std::for_each(std::begin(v4), std::end(v4), [](auto x) {std::cout << x << " "; });
+	std::cout << std::endl;
+	
+
+	std::cout << "19 task" << std::endl;
+	auto a=std::distance(std::begin(v4), std::find(v4.begin(), v4.end(), 1));
+	auto b=std::distance(std::begin(v4), std::upper_bound(v4.begin(), v4.end(), 1));
+	//std::cout<<std::binary_search(v4.begin(), v4.end(), 65);
+	std::cout << "[" << a << "," << b << "]" << std::endl;
+
 
 	return EXIT_SUCCESS;
+
 }
