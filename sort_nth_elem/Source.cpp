ce0bdbd223a1ae25a7b99981ba0e310f16aed9e1
Kamil Garifullin <kamil_20.03@mail.ru>
2021-03-01 20:05:12 +0300

    new version

--- sort_nth_elem/Source.cpp
+++ sort_nth_elem/Source.cpp
@@ -8,7 +8,7 @@
 #include <chrono>
 int main(int argc, char** argv)
 {
-	int N = 1000;
+	int N = 1000000;
 	srand(4541);
 	std::vector <int> vec1;
 	std::vector<int> vec2;
@@ -21,14 +21,14 @@ int main(int argc, char** argv)
 	}
 	std::cout << "sort" << std::endl;
 	int count1 = 0;
-	std::sort(std::begin(vec1), std::end(vec1), [&count1](auto x, auto y) {if (x < y)count1++; return x > y; });
-	std::for_each(std::begin(vec1), std::end(vec1), [](auto x) {std::cout << x << " "; });
+	std::sort(std::begin(vec1), std::end(vec1), [&count1](auto x, auto y) {count1++; return x > y; });
+	//std::for_each(std::begin(vec1), std::end(vec1), [](auto x) {std::cout << x << " "; });
 	std::cout <<"count1 "<<count1<< std::endl;
 
 	std::cout << "nth_element" << std::endl;
 	int count2 = 0;
-	std::nth_element(std::begin(vec2), std::next(std::begin(vec2), 100), std::end(vec2), [&count2](auto x, auto y) {if (x < y)count2++; return x > y; });
-	/*std::for_each(std::begin(vec2), std::end(vec2), [](auto x) {std::cout << x << " "; });*/
+	std::nth_element(std::begin(vec2), std::next(std::begin(vec2), 10), std::end(vec2), [&count2](auto x, auto y) {count2++; return x > y; });
+	//std::for_each(std::begin(vec2), std::end(vec2), [](auto x) {std::cout << x << " "; });
 	std::cout << "count2 "<<count2<<std::endl;
 
 }
