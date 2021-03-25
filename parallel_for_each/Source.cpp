13d4dc3b72bfa42b1d9899a5abac36380c2c8036
Kamil Garifullin <kamil_20.03@mail.ru>
2021-03-25 13:36:15 +0300

    parall_std_for_each

---
+++ parallel_for_each/Source.cpp
@@ -0,0 +1,95 @@
+#include <algorithm>
+#include <future>
+#include <iostream>
+#include <numeric>
+#include <thread>
+#include <vector>
+
+#include<iostream>
+#include<chrono>
+
+#include<algorithm>
+
+#include <random>
+
+#include <functional>
+
+
+class Timer
+{
+public:
+	using clock_t = std::chrono::steady_clock;
+	using time_point_t = clock_t::time_point;
+	void pause() 
+	{
+		pa = clock_t::now();
+		dur += std::chrono::duration_cast<std::chrono::microseconds>(
+			pa - m_begin);
+	}
+	void cont()
+	{
+		m_begin = clock_t::now();
+	}
+	Timer() : m_begin(clock_t::now()), dur(0) {}
+	~Timer()
+	{
+		auto end = clock_t::now();
+		std::cout << "micro: " << std::chrono::duration_cast<std::chrono::microseconds>(
+			end - m_begin).count() + dur.count() << std::endl;
+	}
+private:
+	time_point_t m_begin;
+	time_point_t pa;
+	std::chrono::microseconds dur;
+};
+
+
+template < typename Iterator, typename T >
+void parallel_for_each(Iterator first, Iterator last, T init)
+{
+	const std::size_t length = std::distance(first, last);
+
+	const std::size_t max_size = 25;
+
+	if (length <=  max_size)
+	{
+		std::for_each(first, last, init);
+	}
+	else
+	{
+		Iterator middle = first;
+		std::advance(middle, length / 2);
+
+		std::future < void> first_half_result =
+			std::async(parallel_for_each < Iterator, T >, first, middle, init);
+
+		
+
+		first_half_result.get();
+		parallel_for_each(middle, last, init);
+	}
+}
+
+int main(int argc, char** argv)
+{
+	
+	
+	std::vector < int > v(1000000);
+
+	std::iota(v.begin(), v.end(), 1);
+	
+	std::vector<int> nums{ 3, 4, 2, 8, 15, 267 };
+
+	Timer T;
+	//std::for_each(nums.begin(), nums.end(), [](int& n) { n++; });
+	//std::for_each(std::begin(vector), std::end(vector), [](auto x) {std::cout << x << " "; });
+	//std::for_each(nums.begin(), nums.end(), [](auto x) {std::cout << x*1.34 << " "; });
+	//std::for_each  (v.begin(), v.end(), [](auto x) {std::cout << x << " "; });
+	//parallel_for_each(v.begin(), v.end(), [](auto x) {std::cout << x << " "; });
+	std::for_each(v.begin(), v.end(), [](auto x) {return x * 3.12; });
+	//parallel_for_each(v.begin(), v.end(), [](auto x) {return x*3.12; });
+	//std::cout << parallel_for_each(nums.begin(), nums.end(), [](int& n) { n++; }) << std::endl;
+
+
+	return EXIT_SUCCESS;
+}
