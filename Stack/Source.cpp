de3bff172aa366f28381cc1b47735730d93f3c2a
Kamil Garifullin <kamil_20.03@mail.ru>
2021-03-31 22:56:50 +0300

    3000000 micro

--- Stack/Source.cpp
+++ Stack/Source.cpp
@@ -13,8 +13,6 @@
 #include <mutex>
 #include <stack>
 #include <stdexcept>
-//#include <boost/thread/thread.hpp>
-//#include <boost/atomic.hpp>
 template < typename T >
 class Threadsafe_Stack
 {
@@ -80,49 +78,30 @@ private:
 
     mutable std::mutex m_mutex;
 };
-
 std::atomic<bool> go(false);
-
 struct Life 
 {
     int x, y, z;
 };
-//read_values values1[loop];
-//read_values values2[loop];
-//read_values values3[loop];
-void trigonom(Threadsafe_Stack<int> stack)
+void trigonom(Threadsafe_Stack<int> &stack)
 {
     while (!go)
         std::this_thread::yield();
     for (unsigned i = 0; i < 100000; ++i)
     {
-        /*life.x = sin(life.x) + cos(life.x);
-        std::this_thread::yield();*/
+       
         stack.push(5);
     }
 }
-void read_vals(Threadsafe_Stack<int> stack)
+void read_vals(Threadsafe_Stack<int> &stack)
 {
     while (!go) 
         std::this_thread:: yield();
-   
-        //local=local+ life.x + life.y + life.z;
+ 
         stack.pop();
         std::this_thread::yield();
     
 }
-//void print(read_values* v)
-//{
-//    for (unsigned i = 0; i < loop; ++i)
-//    {
-//        if (i)
-//            std::cout << ",";
-//    std:: cout << "(" << v[i].x << "," << v[i].y << "," << v[i].z << ")";
-//    }
-//    std::cout << std::endl;
-//}
-
-
 class Timer
 {
 public:
@@ -161,69 +140,15 @@ int main(int argc, char* argv[])
     std::atomic<double> local = 0.0;
     for (std::size_t i = 0; i < std::thread::hardware_concurrency(); ++i)
     {
-        threads.push_back(std::thread(trigonom, stack));
+        threads.push_back(std::thread(trigonom, std::ref(stack)));
     }
 
     for (std::size_t i = 0; i < std::thread::hardware_concurrency(); ++i)
     {
-        threads.push_back(std::thread(read_vals, stack));
+        threads.push_back(std::thread(read_vals, std::ref(stack)));
     }
     Timer T;
     go = true;
     std::for_each(threads.begin(), threads.end(),
         std::mem_fn(&std::thread::join));
-    //std::cout << local<<std::endl; 
-    
-    //std::cout << 4 * local2 / N / std::thread::hardware_concurrency();
-    //std::thread t1(increment, &x, values1);
-    //std::thread t2(increment, &y, values2);
-    //std::thread t3(read_vals, values3);
-    //go = true;
-    //t3.join();
-    //t2.join();
-    //t1.join();
-   /* print(values1);
-    print(values2);
-    print(values3);*/
-
-    //int N = 10000000;
-    /*double local = 0;
-    posl_pi(N, local);
-    std::cout << 4 * local / N<<std::endl;*/
-    //std::atomic<double> local2 = 0.0;
-    //double local2 = 0;
-   /* std::vector < std::thread > threads;
-
-    for (std::size_t i = 0; i < std::thread::hardware_concurrency(); ++i)
-    {
-        threads.push_back(std::thread(posl_pi, N, std::ref(local2)));
-    }
-    std::for_each(threads.begin(), threads.end(),
-        std::mem_fn(&std::thread::join));
-
-    std::cout << 4 * local2 / N / std::thread::hardware_concurrency();*/
-
-
 }
-
-//int main()
-//{
-//	std::vector<int>v;
-//	std::vector<int>b;
-//	std::vector<int>c;
-//	v.reserve(100000);
-//	b.reserve(100000);
-//	c.reserve(100000);
-//	for (auto i = 100000; i >= 0; --i)
-//	{
-//		v.push_back(i);
-//		b.push_back(i);
-//		c.push_back(i);
-//	}
-//	Timer T;
-//	std::sort(std::begin(v), std::end(v));
-//	T.pause();
-//	std::sort(std::begin(b), std::end(b));
-//	T.cont();
-//	std::sort(std::begin(c), std::end(c));
-//}
